af1c526d37d252020b7459665b13e8afb4294557
Ben Clarke <ben@arrayofstars.com>
2022-12-02 21:52:43 -0500

    Day 1

---
+++ src/day1.rs
@@ -0,0 +1,42 @@
+const INPUT: &str = include_str!("input_day1.txt");
+
+fn get_carry_amounts(buf: &mut Vec<usize>) {
+    let lines = INPUT.split('\n').map(|x| x.trim()).collect::<Vec<&str>>();
+
+    buf.truncate(0);
+    buf.push(0);
+    let mut idx = 0;
+
+    for line in &lines {
+        if let Ok(val) = line.parse::<usize>() {
+            buf[idx] += val;
+        } else {
+            buf.push(0);
+            idx += 1;
+        }
+    }
+
+    buf.sort();
+}
+
+fn total_carry(buf: &Vec<usize>, n_elves: usize) -> usize {
+    let len = buf.len() - n_elves;
+    buf[len..].iter().sum()
+}
+
+fn tally(n_elves: usize) -> usize {
+    let mut buf: Vec<usize> = vec![];
+
+    get_carry_amounts(&mut buf);
+    total_carry(&buf, n_elves)
+}
+
+pub fn part1() {
+    let max = tally(1);
+    println!("The top elf is carrying {}", max);
+}
+
+pub fn part2() {
+    let max = tally(3);
+    println!("The top three elves are carrying {}", max);
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use aoc_2022::day1;
+
+fn main() {
+    day1::part1();
+    day1::part2();
+}
