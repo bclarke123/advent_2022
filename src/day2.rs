909c7e0000553696f960f53366e996f41cfc1d59
Ben Clarke <ben@arrayofstars.com>
2022-12-02 23:20:28 -0500

    Day 2

---
+++ src/day2.rs
@@ -0,0 +1,73 @@
+const INPUT: &str = include_str!("input_day2.txt");
+
+fn play_for_str(s: &str) -> i32 {
+    match s.chars().next().unwrap() {
+        'A' | 'X' => 1,
+        'B' | 'Y' => 2,
+        _ => 3,
+    }
+}
+
+fn guess_for_play(your_hand: i32, outcome: &str) -> i32 {
+    match (your_hand, outcome.chars().next().unwrap()) {
+        (x, 'Y') => x,
+        (1, 'X') => 3,
+        (1, 'Z') => 2,
+        (2, 'X') => 1,
+        (2, 'Z') => 3,
+        (3, 'X') => 2,
+        (3, 'Z') => 1,
+        _ => panic!(),
+    }
+}
+
+fn bonus(your_hand: i32, my_hand: i32) -> i32 {
+    match (your_hand, my_hand) {
+        (x, y) if x == y => 3,
+        (1, 2) => 6,
+        (1, 3) => 0,
+        (2, 3) => 6,
+        (2, 1) => 0,
+        (3, 1) => 6,
+        (3, 2) => 0,
+        _ => panic!(),
+    }
+}
+
+fn tally<F>(cb: F)
+where
+    F: Fn(&str, &str) -> i32,
+{
+    let lines = INPUT.split('\n').map(|x| x.trim()).collect::<Vec<&str>>();
+
+    let mut score = 0;
+
+    for line in &lines {
+        let mut split = line.split(' ');
+        let your_guess = split.next().unwrap();
+        let my_guess = split.next().unwrap();
+        let play_score = cb(your_guess, my_guess);
+        score += play_score;
+    }
+
+    println!("{}", score);
+}
+
+pub fn part1() {
+    tally(|your_guess, my_guess| {
+        let your_hand = play_for_str(your_guess);
+        let my_hand = play_for_str(my_guess);
+        let bonus = bonus(your_hand, my_hand);
+
+        my_hand + bonus
+    });
+}
+
+pub fn part2() {
+    tally(|your_guess, my_guess| {
+        let your_hand = play_for_str(your_guess);
+        let my_hand = guess_for_play(your_hand, my_guess);
+        let bonus = bonus(your_hand, my_hand);
+        my_hand + bonus
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use aoc_2022::day1;
+use aoc_2022::day2;
 
 fn main() {
+    println!();
+    println!("~~ DAY 1 ~~");
+
     day1::part1();
     day1::part2();
+
+    println!();
+    println!("~~ DAY 2 ~~");
+
+    day2::part1();
+    day2::part2();
 }
